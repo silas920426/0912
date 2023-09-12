c891b4a230f8664e8b63afa5c40662373b9d8b54
silas920426 <silas920426@gmail.com>
2023-09-12 13:23:09 +0800

    01

---
+++ ConsoleApp1/Drink.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Drink
+    {
+        public String Name { get; set; }
+        public String Size { get; set; }
+        public int Price { get; set; }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,34 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Hello, World!");
+        private static void Main(string[] args)
+        {
+
+
+
+            List<Drink> drinks = new List<Drink>();
+            drinks.Add(new Drink() { Name = "紅茶", Size = "大杯", Price = 60 });
+            drinks.Add(new Drink() { Name = "紅茶", Size = "小杯", Price = 40 });
+            drinks.Add(new Drink() { Name = "綠茶", Size = "大杯", Price = 60 });
+            drinks.Add(new Drink() { Name = "綠茶", Size = "小杯", Price = 40 });
+            drinks.Add(new Drink() { Name = "可樂", Size = "大杯", Price = 30 });
+            drinks.Add(new Drink() { Name = "可樂", Size = "小杯", Price = 20 });
+
+            // for-loop
+            //for (int i = 0; i < drinks.Count; i++)
+            //{
+            //    Console.WriteLine($"{drinks[i].Name}  {drinks[i].Size} {drinks[i].Price}元");
+            //}
+
+            // foreach
+            foreach (Drink drink in drinks)
+            {
+                Console.WriteLine($"{drink.Name}  {drink.Size} {drink.Price}元");
+            }
+        }
     }
 }
