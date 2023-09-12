98e8fbaa14d08b68856b86c461a297bf3347b4fc
silas920426 <silas920426@gmail.com>
2023-09-12 14:46:19 +0800

    final

---
+++ ConsoleApp1/OrderItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class OrderItem
+    {
+        public int Index { get; set; }
+        public int Quantity { get; set; }
+        public int SubTotal { get; set; }
+    }
+}
